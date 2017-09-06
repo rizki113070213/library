b831a2fe21154925c2a6c298af4b1d172967c557
rizki113070213 <rizki113070213@gmail.com>
2017-09-06 21:23:32 +0700

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get 'hello_world', to: 'hello_world#index'
-  root 'books#index'
+  root 'hello_world#index'
+  get 'books/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
